287f6f46b5358ae85294638803094da96b1bab56
Lynne Bones <lbones@oa-k3.msk.21-school.ru>
2022-01-26 16:25:40 +0300

    script

---
+++ clean.sh
@@ -0,0 +1,14 @@
+# !/bin/bash
+rm -rf ~/Library/Caches
+rm -rf ~/Library/Application\ Support/Code/Cache
+rm -rf ~/Library/Application\ Support/Code/CachedData
+rm -rf ~/Library/Application\ Support/Code/CachedExtension
+rm -rf ~/Library/Application\ Support/Code/CachedExtensions
+rm -rf ~/Library/Application\ Support/Code/CachedExtensionVSIXs
+rm -rf ~/Library/Application\ Support/Code/Code\ Cache
+rm -rf ~/Library/Application\ Support/Slack/Cache
+rm -rf ~/Library/Application\ Support/Slack/Code\ Cache
+rm -rf ~/Library/Application\ Support/Slack/Service\ Worker/CacheStorage
+rm -rf ~/Library/Application\ Support/Spotify/PersistentCache
+rm -rfv ~/Library/Logs
+echo "-------DONE---------"
